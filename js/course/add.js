b0cb432fab6e61c1af785ec12c02836cf555871e
PengfeiGuo <626138959@qq.com>
2017-05-17 07:49:36 +0800

    创建课程

---
+++ js/course/add.js
@@ -0,0 +1,18 @@
+define(['header', 'aside', 'util', 'nprogress', 'jquery_form', 'jquery'], function(ud, ud, util, nprogress, ud, $) {
+
+	// util返回每一个方法的返回值，想用那个用那个，不用拉到
+	var returns = util({
+		'checkLoginStatus': [],
+		'loading': []
+	});
+	
+	/**
+	 * 创建课程
+	 * */
+	$('form').ajaxForm(function() {
+		location.href = '/html/course/course_add_step1.html';
+	});
+	
+	// 销毁网站加载进度条
+	nprogress.done();
+});
